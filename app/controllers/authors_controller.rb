546f3784de958afb8ab0525f814a564ddc26763f
Camilla Poushyk <camilla.poushyk@gmail.com>
2022-09-21 11:37:47 -0400

    commit tree 961c6e3233dc2049497bd744cfac3fb6121a471c

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,7 @@
 class AuthorsController < ApplicationController
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
   
   def show
     author = Author.find(params[:id])
@@ -7,8 +10,7 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
   end
 
@@ -18,4 +20,12 @@ class AuthorsController < ApplicationController
     params.permit(:email, :name)
   end
   
+  def render_not_found_response
+    render json: {errors: "Author not found"}, status: :not_found
+  end
+
+  def render_unprocessable_entity(invalid)
+      render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+  end
+
 end
